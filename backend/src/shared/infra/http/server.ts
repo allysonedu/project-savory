c3d6c77540c849a0e9ded0ddbffa42052077ed34
allysonedu <allysonedu157@gmail.com>
2025-03-05 17:51:40 -0300

    user creation, creation of the users table and token, connection to the database, .env, use of prism.

---
+++ backend/src/shared/infra/http/server.ts
@@ -0,0 +1,45 @@
+import express, {
+  Request,
+  Response,
+  ErrorRequestHandler,
+  NextFunction,
+} from "express";
+import { logger } from "../../helpers/logger";
+import { AppError } from "../../helpers/errors/AppError";
+
+import { env } from "../../environments/env";
+import routes from "../routes";
+
+const app = express();
+app.use(express.json());
+
+app.get("/health", (_: Request, response: Response) => {
+  logger.debug("Health Check");
+
+  response.status(200).json({
+    status: 200,
+    message: "App ONLINE",
+  });
+});
+
+app.use(routes);
+
+app.use(
+  (err: Error, _request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+      response.status(err.statusCode).json({
+        status: "error",
+        message: err.message,
+      });
+    }
+
+    response.status(500).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+);
+
+app.listen(env.PORT, () => {
+  logger.info(`API rodando na porta ${env.PORT}`);
+});
